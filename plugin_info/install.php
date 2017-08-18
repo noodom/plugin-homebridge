1505f8057ebb6f3742c28d4ee2d80c17e2aa6a30
NebzHB <nebz@nebz.be>
2017-08-18 15:17:07 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -66,17 +66,17 @@ function homebridge_install(){
 		
 		$log_mobile = log::getLogLevel('mobile');
 		$log_homebridge = log::getLogLevel('homebridge');
-		log::add('homebridge', 'info', 'B Log mobile:'.$log_mobile.' '.config::byKey('log::level::mobile'));
-		log::add('homebridge', 'info', 'B Log homebridge:'.$log_homebridge.' '.config::byKey('log::level::homebridge'));
+		log::add('homebridge', 'info', 'B Log mobile:'.$log_mobile.' '.print_r(config::byKey('log::level::mobile')));
+		log::add('homebridge', 'info', 'B Log homebridge:'.$log_homebridge.' '.print_r(config::byKey('log::level::homebridge')));
 		config::save('log::level::homebridge',$log_mobile);
 		$log_mobile = log::getLogLevel('mobile');
 		$log_homebridge = log::getLogLevel('homebridge');
-		log::add('homebridge', 'info', 'A Log mobile:'.$log_mobile.' '.config::byKey('log::level::mobile'));
-		log::add('homebridge', 'info', 'A Log homebridge:'.$log_homebridge.' '.config::byKey('log::level::homebridge'));
+		log::add('homebridge', 'info', 'A Log mobile:'.$log_mobile.' '.print_r(config::byKey('log::level::mobile')));
+		log::add('homebridge', 'info', 'A Log homebridge:'.$log_homebridge.' '.print_(config::byKey('log::level::homebridge')));
 		
 		// + copy data directory*/	
-		$platform_homebridge = dirname(__FILE__).'/data/otherPlatform.json';
-		$platform_mobile = dirname(__FILE__).'/../mobile/data/otherPlatform.json';
+		$platform_homebridge = dirname(__FILE__).'/../data/otherPlatform.json';
+		$platform_mobile = dirname(__FILE__).'/../../mobile/data/otherPlatform.json';
 
 		log::add('homebridge','info','my exists '.$platform_homebridge.' ? '.file_exists($platform_homebridge));
 		if(file_exists($platform_homebridge)) log::add('homebridge','info','my dateM ? '.filemtime($platform_homebridge));
