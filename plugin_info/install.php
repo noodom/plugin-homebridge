07ff11ddfa034d08f7207ce8a028021b8d8d4d71
NebzHB <nebz@nebz.be>
2017-08-18 13:19:23 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -27,9 +27,34 @@ function homebridge_install(){
 	if(mobileExists) {
 		log::add('homebridge', 'debug', 'Présence du plugin Mobile [High Five old friend]');
 		$pluginMobile->deamon_stop();
+		
+		$user_homebridge = config::byKey('user_homebridge','homebridge');
+		log::add('homebridge', 'debug', 'my User:'.$user_homebridge);
+		$user_mobile = config::byKey('user_homebridge','mobile');
+		log::add('homebridge', 'debug', 'mobile User:'.$user_mobile);
+		
+		//config::save('user_homebridge',$user_homebridge,'homebridge');
+
+		$pin_homebridge = config::byKey('pin_homebridge','homebridge');
+		log::add('homebridge', 'debug', 'my pin:'.$pin_homebridge);
+		$pin_mobile = config::byKey('pin_homebridge','mobile');
+		log::add('homebridge', 'debug', 'mobile pin:'.$pin_mobile);
+		//config::save('pin_homebridge',$pin_homebridge,'homebridge');
+
+		/*$name_homebridge = config::byKey('name_homebridge','homebridge',config::byKey('name'),true);
+		log::add('homebridge', 'debug', 'Présence du plugin Mobile [High Five old friend]');
+		config::save('name_homebridge',$name_homebridge,'homebridge');
+
+		$mac_homebridge = config::byKey('mac_homebridge','homebridge',self::generateRandomMac(),true);
+		log::add('homebridge', 'debug', 'Présence du plugin Mobile [High Five old friend]');
+		config::save('mac_homebridge',$mac_homebridge,'homebridge');
+
+		config::save('log::level::homebridge',config::byKey('log::level::mobile'));*/
+		
 	}
 	$pluginHomebridge = plugin::byId('homebridge');
 	$pluginHomebridge->dependancy_install(true);
+	
 }
 
 function homebridge_update(){
