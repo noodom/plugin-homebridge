48deeb5779f64edf74b3761b33558fde0f759da8
Nebz <28622481+NebzHB@users.noreply.github.com>
2018-03-22 11:36:28 +0100

    pre-push

--- resources/install_homebridge.sh
+++ resources/install_homebridge.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PROGRESS_FILE=/tmp/jeedom/homebridge/dependance
+installVer='8' 	#NodeJS major version to be installed
+minVer='8'	#min NodeJS major version to be accepted
+
 touch ${PROGRESS_FILE}
 echo 0 > ${PROGRESS_FILE}
 echo "--0%"
@@ -11,9 +14,10 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 sudo chown -R www-data $DIRECTORY
 
+echo 10 > ${PROGRESS_FILE}
+echo "--10%"
 echo "Lancement de l'installation/mise à jour des dépendances homebridge"
 sudo killall homebridge &>/dev/null
-
 
 if [ -f /etc/apt/sources.list.d/deb-multimedia.list* ]; then
   echo "Vérification si la source deb-multimedia existe (bug lors du apt-get update si c'est le cas)"
@@ -52,120 +56,150 @@ if [ -f /etc/apt/sources.list.d/jeedom.list* ]; then
     fi
   fi
 fi
-echo 5 > ${PROGRESS_FILE}
-echo "--5%"
+
+echo 20 > ${PROGRESS_FILE}
+echo "--20%"
 sudo apt-get update
+yes | sudo dpkg --configure -a
 sudo apt-get install -y avahi-daemon avahi-discover avahi-utils libnss-mdns libavahi-compat-libdnssd-dev
-echo 10 > ${PROGRESS_FILE}
-echo "--10%"
+
+echo 30 > ${PROGRESS_FILE}
+echo "--30%"
 type nodejs &>/dev/null
 if [ $? -eq 0 ]; then actual=`nodejs -v`; fi
 echo "Version actuelle : ${actual}"
 arch=`arch`;
 
-#testVer=`php -r "echo version_compare('${actual}','v6','>=');"`
-#if [[ $testVer == "1" ]]
-if [[ $actual == "v8."* || $actual == "v9."* || $actual == "v10."* ]]
+testVer=`php -r "echo version_compare('${actual}','v${minVer}','>=');"`
+if [[ $testVer == "1" ]]
 then
   echo "Ok, version suffisante";
+  new=$actual
 else
-  echo 20 > ${PROGRESS_FILE}
-  echo "--20%"
+  echo 40 > ${PROGRESS_FILE}
+  echo "--40%"
   echo "KO, version obsolète à upgrader";
   echo "Suppression du Nodejs existant et installation du paquet recommandé"
+  #if npm exists
   type npm &>/dev/null
   if [ $? -eq 0 ]; then
-    sudo npm rm -g homebridge-camera-ffmpeg --save
-    sudo npm rm -g homebridge-jeedom --save
-    sudo npm rm -g homebridge --save
-    sudo npm rm -g request --save
-    sudo npm rm -g node-gyp --save
+    sudo npm rm -g homebridge-camera-ffmpeg --save &>/dev/null
+    sudo npm rm -g homebridge-jeedom --save &>/dev/null
+    sudo npm rm -g homebridge --save &>/dev/null
+    sudo npm rm -g request --save &>/dev/null
+    sudo npm rm -g node-gyp --save &>/dev/null
     cd `npm root -g`;
-    sudo npm rebuild
+    sudo npm rebuild &>/dev/null
+    npmPrefix=`npm prefix -g`
+  else
+    npmPrefix="/usr"
   fi
-  sudo rm -f /usr/bin/homebridge &>/dev/null
-  sudo rm -f /usr/local/bin/homebridge &>/dev/null
-  
   sudo apt-get -y --purge autoremove nodejs npm
-  echo 30 > ${PROGRESS_FILE}
-  echo "--30%"
   
+  echo 45 > ${PROGRESS_FILE}
+  echo "--45%"
   if [[ $arch == "armv6l" ]]
   then
-    echo "Raspberry 1 détecté, utilisation du paquet pour armv6l"
-    sudo rm -f /etc/apt/sources.list.d/nodesource.list &>/dev/null
-    wget http://node-arm.herokuapp.com/node_latest_armhf.deb
-    sudo dpkg -i node_latest_armhf.deb
-    sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
-    rm node_latest_armhf.deb
-    
+    echo "Raspberry 1, 2 ou zéro détecté, utilisation du paquet v${installVer} pour ${arch}"
+    wget https://nodejs.org/download/release/latest-v${installVer}.x/node-*-linux-${arch}.tar.gz
+    tar -xvf node-*-linux-${arch}.tar.gz
+    cd node-*-linux-${arch}
+    sudo cp -R * /usr/local/
+    cd ..
+    rm -fR node-*-linux-${arch}*
+    #upgrade to recent npm
+    sudo npm install -g npm
   else
     echo "Utilisation du dépot officiel"
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-key update
+    curl -sL https://deb.nodesource.com/setup_${installVer}.x | sudo -E bash -
+    wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
     sudo apt-get install -y nodejs  
   fi
+  
+  npm config set prefix ${npmPrefix}
 
   new=`nodejs -v`;
   echo "Version actuelle : ${new}"
 fi
 
-echo 40 > ${PROGRESS_FILE}
-echo "--40%"
-echo "Installation de node-gyp..."
-sudo npm install -g node-gyp
-echo "Installation de request..."
-sudo npm install -g request
 echo 50 > ${PROGRESS_FILE}
 echo "--50%"
-nodePath=`npm root -g`
-sudo rm -Rf ${nodePath}/homebridge-jeedom/.git
+# Remove old globals
+sudo rm -f /usr/bin/homebridge &>/dev/null
+sudo rm -f /usr/local/bin/homebridge &>/dev/null
+sudo npm rm -g homebridge-camera-ffmpeg --save &>/dev/null
+sudo npm rm -g homebridge-jeedom --save &>/dev/null
+sudo npm rm -g homebridge --save &>/dev/null
+sudo npm rm -g request --save &>/dev/null
+sudo npm rm -g node-gyp --save &>/dev/null
+cd `npm root -g`;
+sudo npm rebuild &>/dev/null
+cd ${BASEDIR};
+#remove old local modules
+sudo rm -rf node_modules
+
 echo 60 > ${PROGRESS_FILE}
 echo "--60%"
 echo "Installation de Homebridge..."
-sudo npm install -g --unsafe-perm https://github.com/NebzHB/homebridge-stable.git#master
-echo 70 > ${PROGRESS_FILE}
-echo "--70%"
-echo "Installation de Homebridge-Jeedom..."
 if [ -n $2 ]; then
 	BRANCH=$2
-	echo "Sur la branche ${BRANCH}"
+else
+	BRANCH="master"
 fi
-sudo npm install -g --unsafe-perm https://github.com/NebzHB/homebridge-jeedom.git#${BRANCH}
-echo "Installation de Homebridge-Camera-FFMPEG..."
-sudo npm install -g --unsafe-perm https://github.com/NebzHB/homebridge-camera-ffmpeg.git#master
+sudo sed -i "/.*homebridge-jeedom.*/c\    \"homebridge-jeedom\": \"NebzHB/homebridge-jeedom#${BRANCH}\"," ./package.json
+#need to be sudoed because of recompil
+sudo npm install
+sudo chown -R www-data .
+
+echo 70 > ${PROGRESS_FILE}
+echo "--70%"
+hasPHP7GMP=`sudo dpkg -l | grep php7.0-gmp | wc -l`
+hasPHP5GMP=`sudo dpkg -l | grep php5-gmp | wc -l`
+testGMP=`php -r "echo extension_loaded('gmp');"`
+
+if [[ "$hasPHP5GMP" == "0" ]] && [[ "$hasPHP7GMP" == "0" ]] && [[ "$testGMP" != "1" ]]; then
+  echo "Installation de GMP (génération QRCode)"
+  sudo apt-get -y install php7.0-gmp &>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "pour php5"
+    sudo apt-get -y install php5-gmp
+  else
+    echo "pour php7"
+  fi
+
+  sudo service nginx status &>/dev/null
+  if [ $? = 0 ]; then
+    echo "Reload nginx..."
+    sudo service nginx reload
+  fi
+  sudo service apache2 status &>/dev/null
+  if [ $? = 0 ]; then
+    echo "Reload apache2..."
+    sudo service apache2 reload
+  fi
+fi
+
 echo 80 > ${PROGRESS_FILE}
 echo "--80%"
-
-cd ${BASEDIR}/../node/
-sudo npm cache verify
-sudo rm -rf node_modules
-sudo npm install --unsafe-perm bases &>/dev/null
-sudo npm install --unsafe-perm bignum &>/dev/null
-
-# do not break i don't know what
-#if [[ `file -bi /usr/bin/ffmpeg` == *"application/x-executable"* ]]; then 
-#  echo "FFMPEG existe"; 
-#else 
-#  echo "FFMPEG n'existe pas, on copie le wrapper pour avconv"; 
-#  sudo cp -f ${nodePath}/homebridge-jeedom/ffmpeg-wrapper /usr/bin/ffmpeg
-#  sudo chmod +x /usr/bin/ffmpeg
-#fi
+# removing old node solution
+if [ -e ${BASEDIR}/../node ]; then
+  cd ${BASEDIR}/../node/
+  sudo npm cache verify
+  cd ${BASEDIR}/../
+  sudo rm -Rf ${BASEDIR}/../node
+fi
+# cleaning of past tries
 if [[ `file -bi /usr/bin/ffmpeg` == *"text/x-shellscript"* ]]; then 
   echo "Nettoyage de mon wrapper FFMPEG"; 
   sudo rm -f /usr/bin/ffmpeg
   echo "Réinstallez ffmpeg s'il était installé"; 
 fi 
 
+
 echo 90 > ${PROGRESS_FILE}
 echo "--90%"
-#sudo systemctl is-enabled avahi-daemon &>/dev/null
-#if [ $? -ne 0 ]; then
-#	echo "avahi-daemon non activé au démarrage, activation..."
-#	sudo systemctl enable avahi-daemon
-	echo "Désactivation de avahi-daemon au démarrage...(il démarrera avec le daemon (on contourne le bug de la Smart du 1 jan 1970))"
-	sudo systemctl disable avahi-daemon &>/dev/null
-#fi
+echo "Désactivation de avahi-daemon au démarrage...(il démarrera avec le daemon (on contourne le bug de la Smart du 1 jan 1970))"
+sudo systemctl disable avahi-daemon &>/dev/null
 sudo sed -i "/.*enable-dbus.*/c\enable-dbus=yes  #changed by homebridge" /etc/avahi/avahi-daemon.conf
 sudo sed -i "/.*use-ipv6.*/c\use-ipv6=no  #changed by homebridge" /etc/avahi/avahi-daemon.conf
 #sudo sed -i "/.*publish-aaaa-on-ipv4.*/c\publish-aaaa-on-ipv4=yes  #changed by homebridge" /etc/avahi/avahi-daemon.conf
@@ -174,6 +208,9 @@ if [ -n $1 ]; then
 	UsedEth=$(ip addr | grep $1 | awk '{print $7}')
 	sudo sed -i "/.*allow-interfaces.*/c\#allow-interfaces=$UsedEth  #changed by homebridge" /etc/avahi/avahi-daemon.conf
 fi
+
+echo 95 > ${PROGRESS_FILE}
+echo "--95%"
 if [ -f /etc/apt/sources.list.d/deb-multimedia.list.disabledByHomebridge ]; then
   echo "Réactivation de la source deb-multimedia qu'on avait désactivé !"
   sudo mv /etc/apt/sources.list.d/deb-multimedia.list.disabledByHomebridge /etc/apt/sources.list.d/deb-multimedia.list
@@ -182,7 +219,8 @@ if [ -f /etc/apt/sources.list.d/jeedom.list.disabledByHomebridge ]; then
   echo "Réactivation de la source repo.jeedom.com qu'on avait désactivé !"
   sudo mv /etc/apt/sources.list.d/jeedom.list.disabledByHomebridge /etc/apt/sources.list.d/jeedom.list
 fi
-echo "Installation Homebridge OK"
+
 echo 100 > ${PROGRESS_FILE}
 echo "--100%"
-rm ${PROGRESS_FILE}
+echo "Installation des dépendances Homebridge terminée, vérifiez qu'il n'y a pas d'erreur"
+rm -f ${PROGRESS_FILE}
